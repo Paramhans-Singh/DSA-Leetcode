835df9a9e9f5df080e3d979dcede917a8ccd7571
Paramhans Singh <paramhanssingh084@gmail.com>
2023-09-19 12:42:25 +0530

    Time: 0 ms (100%), Space: 40.2 MB (16.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,9 +4,7 @@ class Solution {
         int prev = nums[0];
         int prev2 = 0;
       for(int i=1;i<n;i++){
-        int pick = nums[i];
-        if(i>1) pick+=prev2;
-        
+        int pick = nums[i] + prev2;
         int notpick = 0+prev;
 
         int curr = Integer.max(pick, notpick);
