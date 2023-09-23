6df19e88ae185bfc829709d4435938b02231ef51
Paramhans Singh <paramhanssingh084@gmail.com>
2023-09-23 13:39:34 +0530

    Time: 2 ms (46.57%), Space: 41.3 MB (70.09%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,42 +1,26 @@
+
 class Solution {
-    public int myAtoi(String s) {
-        s = s.trim();
-        if(s.length() == 0) return 0;
-        return myAtoi(s, 0, -1, 0);
+  public int myAtoi(String s) {
+    s = s.strip();
+    if (s.isEmpty())
+      return 0;
+
+    final int sign = s.charAt(0) == '-' ? -1 : 1;
+    if (s.charAt(0) == '+' || s.charAt(0) == '-')
+      s = s.substring(1);
+
+    long num = 0;
+
+    for (final char c : s.toCharArray()) {
+      if (!Character.isDigit(c))
+        break;
+      num = num * 10 + (c - '0');
+      if (sign * num <= Integer.MIN_VALUE)
+        return Integer.MIN_VALUE;
+      if (sign * num >= Integer.MAX_VALUE)
+        return Integer.MAX_VALUE;
     }
 
-    private int myAtoi(String s, int i, int sign, int result) {
-        if(i >= s.length()) return result;
-
-        char ch = s.charAt(i);
-        if(sign != -1) {
-            if(Character.isDigit(ch)) {
-                int dig = ch - '0';
-                if(sign == 1) {
-                    if((result < Integer.MIN_VALUE / 10) || ((result == Integer.MIN_VALUE / 10) && dig >= 8))
-                        return Integer.MIN_VALUE;
-                    result = result * 10 - dig;
-                    return myAtoi(s, i + 1, sign, result);
-                }
-                if((result > Integer.MAX_VALUE / 10) || ((result == Integer.MAX_VALUE / 10) && dig >= 7))
-                    return Integer.MAX_VALUE;
-                result = result * 10 + dig;
-                return myAtoi(s, i + 1, sign, result);
-            }
-            return result;
-        } else {
-            if(ch >= '1' && ch <= '9') {
-                int dig = ch - '0';
-                if((result >= Integer.MAX_VALUE / 10) && dig >= 8)
-                    return Integer.MAX_VALUE;
-                result = result * 10 + dig;
-                return myAtoi(s, i + 1, 0, result);
-            }
-            else if(ch == ' ') return myAtoi(s, i + 1, sign, result);
-            else if(ch == '0') return myAtoi(s, i + 1, 0, result);
-            else if(ch == '-') return myAtoi(s, i + 1, 1, result);
-            else if(ch == '+') return myAtoi(s, i + 1, 0, result);
-            else return result;
-        }
-    }
+    return sign * (int) num;
+  }
 }
