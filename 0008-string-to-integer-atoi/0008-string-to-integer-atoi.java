f10cc3d981bc95bac2212ef234094674620b651e
Paramhans Singh <paramhanssingh084@gmail.com>
2023-09-23 14:22:54 +0530

    Time: 1 ms (100%), Space: 40.7 MB (99.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,54 @@
-
 class Solution {
-  public int myAtoi(String s) {
-    s = s.strip();
-    if (s.isEmpty())
-      return 0;
-
-    final int sign = s.charAt(0) == '-' ? -1 : 1;
-    if (s.charAt(0) == '+' || s.charAt(0) == '-')
-      s = s.substring(1);
-
-    long num = 0;
-
-    for (final char c : s.toCharArray()) {
-      if (!Character.isDigit(c))
-        break;
-      num = num * 10 + (c - '0');
-      if (sign * num <= Integer.MIN_VALUE)
-        return Integer.MIN_VALUE;
-      if (sign * num >= Integer.MAX_VALUE)
-        return Integer.MAX_VALUE;
+       public static int myAtoi(String s) {
+        s = s.trim();
+        if(s.length() == 0) return 0;
+        return helper(s, 0, -1, 0);
     }
 
-    return sign * (int) num;
-  }
+    private static int helper(String s, int i, int sign, int result) {
+        if(i >= s.length()) return result;
+
+        char ch = s.charAt(i);
+        if(sign != -1) {
+            if(Character.isDigit(ch)) {
+                int dig = ch - '0';
+                if(sign == 1) {
+                    if((result < Integer.MIN_VALUE/10) || ((result == Integer.MIN_VALUE / 10) && dig >= 8))
+                        return Integer.MIN_VALUE;
+                    else{
+                       result = result * 10 - dig;
+                       return helper(s, i + 1, sign, result);
+                    }    
+                }
+                else{
+                    if((result > Integer.MAX_VALUE/10) || ((result == Integer.MAX_VALUE/10)&& dig >= 7))
+                    return Integer.MAX_VALUE;
+
+                    else{
+                      result = result * 10 + dig;
+                      return helper(s, i + 1, sign, result);
+                    }
+                }   
+            }     
+            return result;
+            }
+        
+        else {
+            if(ch >= '1' && ch <= '9') {
+                int dig = ch - '0';
+                if((result >= Integer.MAX_VALUE/10) && dig >= 8)
+                    return Integer.MAX_VALUE;
+                
+                else{
+                 result = result * 10 + dig;
+                 return helper(s, i + 1, 0, result);
+                }
+            }
+            // else if(ch == ' ') return helper(s, i + 1, sign, result);
+            else if(ch == '0') return helper(s, i + 1, 0, result);
+            else if(ch == '-') return helper(s, i + 1, 1, result);
+            else if(ch == '+') return helper(s, i + 1, 0, result);
+            else return result;
+        }
+    }
 }
