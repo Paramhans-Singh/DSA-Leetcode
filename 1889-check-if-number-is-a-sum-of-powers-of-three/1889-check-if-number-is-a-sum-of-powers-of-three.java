eecf6d11f7962ee0a755cdecf09479b346a5f8fd
Paramhans Singh <paramhanssingh084@gmail.com>
2023-10-24 01:45:06 +0530

    Time: 0 ms (100%), Space: 38.9 MB (86.79%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,13 +1,12 @@
 class Solution { 
     public boolean checkPowersOfThree(int n) {
-        if(n==0){
-            return true;
-        }
-        int x = n%3;
-		//this will give the digit at that particular place in ternary representation;
-        if(x==2){
-            return false;
-        }
+        if(n==0) return true;
+    
+        int x = n % 3;
+//this will give the digit at that particular place in ternary representation
+// if ternary is 2 then it shows repetition of power of 3 at that place 
+        if(x==2) return false;
+
         return checkPowersOfThree(n/3);
     }
 }
