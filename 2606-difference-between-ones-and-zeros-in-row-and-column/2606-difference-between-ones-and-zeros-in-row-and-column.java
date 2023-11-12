09fa4a5a984deda3269933990b67bc99bb8d8752
Paramhans Singh <paramhanssingh084@gmail.com>
2023-11-13 01:39:50 +0530

    Time: 14 ms (24.93%), Space: 71 MB (84.06%) - LeetHub

--- 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
+        int mainRow = grid.length;
+        int mainCol = grid[0].length;
+
         int zeroesRow[] = new int[grid.length];
-        int onesRow[] = new int[grid.length];
+        // int onesRow[] = new int[grid.length];
         int zeroesCol[] = new int[grid[0].length];
-        int onesCol[] = new int[grid[0].length];
+        // int onesCol[] = new int[grid[0].length];
 
         int row = 0;
         while(row<grid.length){
             int zeroes = 0;
-            int ones = 0;
+            // int ones = 0;
 
         for(int col=0; col<grid[0].length; col++){
             if(grid[row][col]==0)zeroes++;
-            else ones++;
+            // else ones++;
         }
         zeroesRow[row] = zeroes;
-        onesRow[row] = ones;
+        // onesRow[row] = ones;
 
         row++;
         }
@@ -23,13 +26,13 @@ class Solution {
         int col1 = 0;
         while(col1<grid[0].length){
             int zeroes = 0;
-            int ones = 0;
+            // int ones = 0;
             for(int row1=0; row1<grid.length; row1++){
                 if(grid[row1][col1]==0)zeroes++;
-                 else ones++;
+                //  else ones++;
                  }
                  zeroesCol[col1] = zeroes;
-                 onesCol[col1] = ones;
+                //  onesCol[col1] = ones;
 
                  col1++;
         }
@@ -37,7 +40,7 @@ class Solution {
         int ans[][] = new int[grid.length][grid[0].length];
         for(int i=0; i<ans.length; i++){
             for(int j=0; j<ans[0].length; j++){
-                ans[i][j] = (onesRow[i]+ onesCol[j]) - (zeroesRow[i]+zeroesCol[j]);
+             ans[i][j] = ((mainRow-zeroesRow[i])+ (mainCol-zeroesCol[j])) - (zeroesRow[i]+zeroesCol[j]);
             }
         }
 
