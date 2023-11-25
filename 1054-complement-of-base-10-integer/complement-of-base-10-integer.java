03301aa86fc7cff133d0c460218b9342e7dc5d6d
Paramhans Singh <paramhanssingh084@gmail.com>
2023-11-25 19:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (5.92%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int ans = 0;
+        int temp = n;
+        int mask = 0;
+
+        if(n==0) return 1;
+
+        while(temp!=0){
+           mask = (mask<<1) | 1;
+           temp = temp>>1;
+        }
+        ans = ~n & mask;
+        return ans;
+    }
+}
