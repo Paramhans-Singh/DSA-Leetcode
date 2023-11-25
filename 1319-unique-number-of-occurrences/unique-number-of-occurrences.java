fb2d36b00437392c647de194c260848c1aa7ef13
Paramhans Singh <paramhanssingh084@gmail.com>
2023-11-26 02:48:49 +0530

    Time: 2 ms (98.43%) | Memory: 40.6 MB (47.70%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+            HashMap<Integer,Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }else{
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (int key : map.keySet()) {
+             set.add(map.get(key));
+        }
+
+        return map.size()==set.size();
+    }
+}
