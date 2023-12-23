977affac69ab47b3a15d221cbe064c81b2af108c
Paramhans Singh <paramhanssingh084@gmail.com>
2023-12-23 11:03:08 +0530

    Time: 1 ms (97.37%) | Memory: 41 MB (5.17%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -2,11 +2,9 @@ class Solution {
     public int reverse(int x) {
         int ans = 0;
         while(x!=0){ // as x can be -ve also 
-            int rem = x%10;
-            
-            if(ans > Integer.MAX_VALUE/10 || ans<Integer.MIN_VALUE/10)return 0;
-            
-            ans = ans *10 + rem;
+        int rem = x%10;  
+        if(ans > Integer.MAX_VALUE/10 || ans<Integer.MIN_VALUE/10)return 0;
+            ans = ans *10 + rem; // inorder to multiply by 10 we have to     check first in the above if by max and min values
             x= x/10;
         }
        
