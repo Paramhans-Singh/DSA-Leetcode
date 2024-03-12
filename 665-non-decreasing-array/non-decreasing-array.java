31a98fe0e56caa8123f7ff11eb02eabe1184756b
Paramhans Singh <paramhanssingh084@gmail.com>
2024-03-13 02:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (48.52%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.java
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean checkPossibility(int[] nums) {
 	for (int i=1, modified=0, prev = nums[0]; i<nums.length; i++) {
 		if (nums[i] < prev) {
-			if (modified++ == 1) return false;
+			if (modified == 1) return false;
+            modified++;
 			if (i>=2 && nums[i-2] > nums[i]) continue;
 		}
 		prev = nums[i];
