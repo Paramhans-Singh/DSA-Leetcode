24cb0ba62fdb17f0d3573feaa14f5348f0592346
Paramhans Singh <paramhanssingh084@gmail.com>
2024-03-14 01:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (81.07%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -5,10 +5,12 @@ class Solution {
         for (int ele : nums)
             totalSum += ele;
 
-        for (int i = 0; i < nums.length; leftsum += nums[i++])
+        for (int i = 0; i < nums.length; i++){
             if (leftsum * 2 == totalSum - nums[i])
-                return i;       
+                return i;  
 
+        leftsum += nums[i];     
+        }
         return -1; 
     }
 }
