81536ba05b1383f239515453e007ffdb8c44a8af
Paramhans Singh <paramhanssingh084@gmail.com>
2024-04-07 02:35:48 +0530

    Time: 846 ms (42.84%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
 SELECT DISTINCT author_id AS id FROM Views WHERE viewer_id = author_id ORDER BY author_id;
+
+--  alias , distinct , order by ka use kara hai 
