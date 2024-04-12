57c6b9e0496ce2641e6b36a9bfcfbb8bede23c36
Paramhans Singh <paramhanssingh084@gmail.com>
2024-04-12 18:36:14 +0530

    Time: 903 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,5 @@
 SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) AS processing_time 
 FROM Activity a1, Activity a2 
-WHERE a1.machine_id=a2.machine_id AND a1.process_id=a2.process_id
+WHERE a1.machine_id=a2.machine_id 
 AND a1.activity_type='start' AND a2.activity_type='end'
 GROUP BY a1.machine_id
